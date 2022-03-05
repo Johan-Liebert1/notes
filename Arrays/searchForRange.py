0c55ae6d7bab67857738c312a519a3e77344199a
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2022-03-05 09:48:10 +0530

    more

---
+++ Arrays/searchForRange.py
@@ -0,0 +1,10 @@
+"""  
+Given: an array of sorted integer and a target number 
+To do: Find, in the array, the index of first occurance of the target number and the index of last occurance
+of the target number
+
+ex = [2, 5, 8, 10, 11, 11, 11, 11, 23, 56], target = 11
+ans = [4, 7]
+
+do this in O(log n) time
+"""
--- Trees/allKindsOfNodeDepths.py
+++ Trees/allKindsOfNodeDepths.py
@@ -18,12 +18,96 @@ depth of 2 = 6 as, depth of 4 = 1, 5 = 1, 8 = 2 and 9 = 2 relative to node 2 so 
 
 from binaryTree import Node
 
+# O(n) time | O(n) space
+def all_kinds_of_node_depths(root: Node):
+    # num nodes in every sub tree
+    node_counts: dict[Node, int] = {}
 
-def all_kinds_of_node_depths(rootNode: Node):
-    array = [0]
+    add_node_counts(root, node_counts)
 
-    depth_calculator(rootNode, array)
+    node_depths: dict[Node, int] = {}
+
+    add_node_depths(root, node_depths, node_counts)
+
+    return node_depths(root, node_depths)
 
 
-def depth_calculator(root: Node, array: "list[int]", depth=0):
-    pass
+def node_depths(rootNode: Node, node_depths: dict[Node, int]):
+    if rootNode is None:
+        return 0
+
+    return (
+        node_depths[rootNode]
+        + all_kinds_of_node_depths(rootNode.left, node_depths)
+        + all_kinds_of_node_depths(rootNode.right, node_depths)
+    )
+
+
+def add_node_depths(
+    node: Node, node_depths: dict[Node, int], node_counts: dict[Node, int]
+):
+    # leaf nodes have a depth of 0
+    node_depths[node] = 0
+
+    if node.left is not None:
+        add_node_depths(node.left, node_depths, node_counts)
+        node_depths[node] += node_depths[node.left] + node_counts[node.left]
+
+    if node.right is not None:
+        add_node_depths(node.right, node_depths, node_counts)
+        node_depths[node] += node_depths[node.right] + node_counts[node.right]
+
+
+def add_node_counts(node: Node, node_counts: dict[Node, int]):
+    # every node has a count of at least 1
+    node_counts[node] = 1
+
+    if node.left is not None:
+        add_node_counts(node.left, node_counts)
+        node_counts[node] += node_counts[node.left]
+
+    if node.right is not None:
+        add_node_counts(node.right, node_counts)
+        node_counts[node] += node_counts[node.right]
+
+
+class TreeInfo:
+    def __init__(
+        self, num_nodes_in_tree: int, sum_of_depths: int, sum_of_all_depths: int
+    ) -> None:
+        self.num_nodes_in_tree: int = num_nodes_in_tree
+        self.sum_of_depths: int = sum_of_depths
+        self.sum_of_all_depths: int = sum_of_all_depths
+
+
+# O(n) time | O(1) space
+def all_kinds_of_node_depths_better_space(root: Node):
+    return get_tree_info(root).sum_of_all_depths
+
+
+def get_tree_info(tree: Node) -> TreeInfo:
+    if tree is None:
+        return TreeInfo(0, 0, 0)
+
+    left_tree_info = get_tree_info(tree.left)
+    right_tree_info = get_tree_info(tree.right)
+
+    num_nodes_in_tree = (
+        1 + left_tree_info.num_nodes_in_tree + right_tree_info.num_nodes_in_tree
+    )
+
+    sum_of_left_depths = left_tree_info.sum_of_depths + left_tree_info.num_nodes_in_tree
+
+    sum_of_right_depths = (
+        right_tree_info.sum_of_depths + right_tree_info.num_nodes_in_tree
+    )
+
+    sum_of_depths = sum_of_left_depths + sum_of_right_depths
+
+    sum_of_all_depths = (
+        sum_of_depths
+        + left_tree_info.sum_of_all_depths
+        + right_tree_info.sum_of_all_depths
+    )
+
+    return TreeInfo(num_nodes_in_tree, sum_of_depths, sum_of_all_depths)
