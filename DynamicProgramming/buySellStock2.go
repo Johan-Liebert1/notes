7c3f65b9bcc20155af28c39012f6e2f96ee85bb3
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2024-11-20 10:47:21 +0530

    ++

--- DynamicProgramming/buySellStock2.go
+++ DynamicProgramming/buySellStock2.go
@@ -5,49 +5,109 @@ func profit(prices []int, index int, lastBought int, cache [][]int) int {
 		return 0
 	}
 
-    state := 0
+	if cache[lastBought][index] != -1 {
+		return cache[lastBought][index]
+	}
 
-    if lastBought != -1 {
-        state = 1
-    }
-
-    if cache[state][index] != -1 {
-        return cache[state][index]
-    }
-
+	// we can either buy or sell
 	buy := 0
 	sell := 0
 
+	// or do nothing
 	nothing := profit(prices, index+1, lastBought, cache)
 
-	// can only buy if we haven't already bought
-	if lastBought == -1 {
-		buy = -prices[index] + profit(prices, index+1, index, cache)
-	}
-
-	if lastBought != -1 {
-		sell = prices[index] + profit(prices, index+1, -1, cache)
+	// can only buy if lastBought == -1
+	if lastBought == 0 {
+		buy = -prices[index] + profit(prices, index+1, 1, cache)
+	} else {
+		sell = prices[index] + profit(prices, index+1, 0, cache)
 	}
 
 	maxProfit := max(buy, sell, nothing)
 
-    cache[state][index] = maxProfit
+	cache[lastBought][index] = maxProfit
 
 	return maxProfit
 }
 
+func maxProfitRecursion(prices []int) int {
+	cache := [][]int{}
+
+	for range 2 {
+		arr := make([]int, len(prices))
+
+		for i := range arr {
+			arr[i] = -1
+		}
+
+		cache = append(cache, arr)
+	}
+
+	return profit(prices, 0, 0, cache)
+}
+
+func maxProfitIterative(prices []int) int {
+	n := len(prices)
+	if n == 0 {
+		return 0
+	}
+
+	// Create a cache table
+	cache := make([][]int, 2)
+
+	for i := 0; i < 2; i++ {
+		cache[i] = make([]int, n+1)
+	}
+
+	// Initialize base case: dp[0][n] and dp[1][n] = 0
+
+    // don't really need to do this cause Go, but keeping it here for reference
+	for i := 0; i < 2; i++ {
+		cache[i][n] = 0
+	}
+
+	// Fill the dp table
+	for index := n - 1; index >= 0; index-- {
+		for lastBought := 0; lastBought < 2; lastBought++ {
+            // simply copy the recurrence relation
+			nothing := cache[lastBought][index+1]
+			buy := 0
+			sell := 0
+
+			if lastBought == 0 {
+				buy = -prices[index] + cache[1][index+1]
+			} else {
+				sell = prices[index] + cache[0][index+1]
+			}
+
+			cache[lastBought][index] = max(buy, sell, nothing)
+		}
+	}
+
+	// The result is stored in dp[0][0]
+	return cache[0][0]
+}
+
+// We can see in the iterative solution that we're only using two values
+// so we don't really need an entire array
 func maxProfit(prices []int) int {
-    cache := make([][]int, 2)
+    // these are like the [lastBought (0 | 1)][index + 1]
+	futureNoStock, futureWithStock := 0, 0
 
-    for i := range 2 {
-        p := make([]int, len(prices))
+	// Iterate backward through the prices
+	for index := len(prices) - 1; index >= 0; index-- {
+		// Compute the current states based on future states
 
-        for j := range len(p) {
-            p[j] = -1
-        }
+        // profit if NO stock is held at index
+		currentNoStock := max(futureNoStock, -prices[index]+futureWithStock)
 
-        cache[i] = p
-    }
+        // profit if SOME stock is held at index
+		currentWithStock := max(futureWithStock, prices[index]+futureNoStock)
 
-	return profit(prices, 0, -1, cache)
+		// Update future states for the next iteration
+		futureNoStock, futureWithStock = currentNoStock, currentWithStock
+	}
+
+	// The result is the max profit starting at index 0 without holding stock
+	return futureNoStock
 }
--- DynamicProgramming/buySellStock3.go
+++ DynamicProgramming/buySellStock3.go
@@ -36,13 +36,97 @@ func buySellStock3(prices []int) int {
 		cache[i] = make([][]int, 3) // for remaining transactions 0, 1, 2
 
 		for j := range cache[i] {
-            cache[i][j] = make([]int, len(prices))
+			cache[i][j] = make([]int, len(prices))
 
-            for k := range cache[i][j] {
-                cache[i][j][k] = -1
-            }
+			for k := range cache[i][j] {
+				cache[i][j][k] = -1
+			}
 		}
 	}
 
 	return profit3(prices, 0, 0, 2, cache)
+}
+
+func buySellStock3DP(prices []int) int {
+	cache := make([][][]int, 2) // for lastBought (0 or 1)
+
+	for i := range 2 {
+		cache[i] = make([][]int, 3) // for remaining transactions 0, 1, 2
+
+		for j := range cache[i] {
+			cache[i][j] = make([]int, len(prices)+1)
+		}
+	}
+
+	for index := len(prices) - 1; index >= 0; index-- {
+		for remainingTransactions := 2; remainingTransactions >= 0; remainingTransactions-- {
+			for lastBought := 0; lastBought <= 1; lastBought++ {
+				// profit3(prices, index+1, lastBought, remainingTransactions, cache)
+				nothing := cache[lastBought][remainingTransactions][index+1]
+
+				buy := 0
+				sell := 0
+
+				if lastBought == 0 && remainingTransactions > 0 {
+					// profit3(prices, index+1, 1, remainingTransactions-1, cache)
+					buy = -prices[index] + cache[1][remainingTransactions-1][index+1]
+				}
+
+				if lastBought != 0 {
+					// profit3(prices, index+1, 0, remainingTransactions, cache)
+					sell = prices[index] + cache[0][remainingTransactions][index+1]
+				}
+
+				profit := max(buy, sell, nothing)
+
+				cache[lastBought][remainingTransactions][index] = profit
+			}
+		}
+	}
+
+	// cache[not bought][transactions completed][day]
+
+	// so basically on the 0th day, nothing bought and 2 transactions completed
+	return cache[0][2][0]
+}
+
+func buySellStock3SpaceOptmisied(prices []int) int {
+	current := make([][]int, 2)
+	next := make([][]int, 2)
+
+	for i := range 2 {
+		current[i] = make([]int, 3)
+		next[i] = make([]int, 3)
+	}
+
+	for index := len(prices) - 1; index >= 0; index-- {
+		for remainingTransactions := 2; remainingTransactions >= 0; remainingTransactions-- {
+			for lastBought := 0; lastBought <= 1; lastBought++ {
+				// profit3(prices, index+1, lastBought, remainingTransactions, cache)
+				nothing := next[lastBought][remainingTransactions]
+
+				buy := 0
+				sell := 0
+
+				if lastBought == 0 && remainingTransactions > 0 {
+					// profit3(prices, index+1, 1, remainingTransactions-1, cache)
+					buy = -prices[index] + next[1][remainingTransactions-1]
+				}
+
+				if lastBought != 0 {
+					// profit3(prices, index+1, 0, remainingTransactions, cache)
+					sell = prices[index] + next[0][remainingTransactions]
+				}
+
+				profit := max(buy, sell, nothing)
+
+				current[lastBought][remainingTransactions] = profit
+			}
+		}
+
+        next = current;
+	}
+
+    // next[haven't bought anything][2 transactions remaining]
+	return next[0][2]
 }
