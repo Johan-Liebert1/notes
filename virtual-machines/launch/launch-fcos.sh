edcf9ead2a272216eb82a7b76d3f94ad14894845
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2026-02-05 14:57:00 +0530

    More changes

--- virtual-machines/launch/launch-fcos.sh
+++ virtual-machines/launch/launch-fcos.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+set -eu
+
 STREAM="stable"
 
-IGNITION_CONFIG="/home/pragyan/notes/virtual-machines/launch/ignition-luks.ign"
+IGNITION_CONFIG="/home/pragyan/notes/virtual-machines/launch/ignition.ign"
 IMAGE="$HOME/notes/bootc/test.img"
 VM_NAME="composefs-only"
 VCPUS="2"
 RAM_MB="2048"
 STREAM="stable"
 DISK_GB="10"
+SECUREBOOT=false
 
 # For x86 / aarch64,
 IGNITION_DEVICE_ARG=(--qemu-commandline="-fw_cfg name=opt/com.coreos/config,file=${IGNITION_CONFIG}")
@@ -44,6 +47,12 @@ while [ ! -z "${1:-}" ]; do
             shift
         ;;
 
+        "--secureboot" )
+            SECUREBOOT=true
+            shift
+        ;;
+
+
         * )
             echo "Argument $1 not understood"
             exit 1
@@ -56,13 +65,16 @@ set -ex
 # Setup the correct SELinux label to allow access to the config
 chcon --verbose --type svirt_home_t ${IGNITION_CONFIG}
 
-mkdir -p ~/.local/share/libvirt/nvram
-cp /usr/share/OVMF/OVMF_VARS.fd ~/.local/share/libvirt/nvram/"${VM_NAME}"_VARS.fd
+# OVMF paths for secure boot
+OVMF_CODE="/usr/share/OVMF/OVMF_CODE.secboot.fd"
+OVMF_VARS="$HOME/.local/share/libvirt/nvram/${VM_NAME}_VARS.fd"
+OVMF_VARS_TEMPLATE="/usr/share/OVMF/OVMF_VARS.secboot.fd"
 
+mkdir -p "$(dirname OVMF_VARS)"
+cp /usr/share/OVMF/OVMF_VARS.fd "$OVMF_VARS"
 
 args=()
-secureboot=false
-if [[ "${secureboot}" == "true" ]]; then
+if [[ "${SECUREBOOT}" == "true" ]]; then
     loader="loader=${OVMF_CODE},loader.readonly=yes,loader.type=pflash"
     nvram="nvram=${OVMF_VARS},nvram.template=${OVMF_VARS_TEMPLATE},loader_secure=yes"
     features="firmware.feature0.name=secure-boot,firmware.feature0.enabled=yes,firmware.feature1.name=enrolled-keys,firmware.feature1.enabled=yes"
@@ -84,14 +96,22 @@ else
     disk=(--disk="size=${DISK_GB},backing_store=${IMAGE}")
 fi
 
+CONNECT="qemu:///session"
+
+if [[ $(whoami) == "root" ]]; then
+    CONNECT="qemu:///system"
+fi
+
 virt-install \
-    --connect="qemu:///session" \
+    --connect="$CONNECT" \
     --name="${VM_NAME}" \
     --vcpus="${VCPUS}" \
     --memory="${RAM_MB}" \
     --os-variant="fedora-coreos-$STREAM" \
     --import --nographics \
     --network bridge=virbr0 \
+    --memorybacking=source.type=memfd,access.mode=shared \
+    --filesystem /var/lib/containers,containers,driver.type=virtiofs \
     "${disk[@]}" \
     "${args[@]}" \
     "${IGNITION_DEVICE_ARG[@]}"
---
+++ virtual-machines/password-change.md
@@ -0,0 +1,5 @@
+# Change password of a qcow2 image
+
+```bash
+sudo virt-customize -a /home/pragyan/.local/share/libvirt/images/fedora.qcow2 --root-password password:hello
+```
